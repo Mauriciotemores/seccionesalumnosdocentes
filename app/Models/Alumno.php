5e56dcd4f019f1f903f2860af1e447d0ddb21520
Mauriciotemores <mauricio.temores5339@alumnos.udg.mx>
2025-04-30 20:06:54 -0600

    Codigo de Alumno y Docente php

--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Alumno extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'matricula'];
+
+    public function secciones()
+    {
+        return $this->belongsToMany(Seccion::class);
+    }
 }
--- app/Models/Docente.php
+++ app/Models/Docente.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Docente extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'cedula'];
+
+    public function secciones()
+    {
+        return $this->belongsToMany(Seccion::class);
+    }
 }
